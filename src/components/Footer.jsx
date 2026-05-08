3c491f59f0faa5828b33183333e7dc65a4cf92d2
GitHub nayoungq99 <GitHub nayoung040317@naver.com>
2026-05-08 22:59:35 +0900

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,41 +1,22 @@
 import { Link } from 'react-router-dom';
 
-function Footer() {
+export default function Footer() {
   return (
-    <footer style={footerWrapperStyle}>
-      <Link to="/cart" style={linkStyle}>
-        <button style={bottomPurchaseButtonStyle}>
+    <footer style={{ display: 'flex', justifyContent: 'center', padding: '40px' }}>
+      <Link to="/cart" style={{ width: '60%' }}>
+        <button style={{
+          width: '100%',
+          padding: '6px',
+          backgroundColor: 'white',
+          border: '2px solid #0085FF',
+          color: '#0085FF',
+          borderRadius: '8px',
+          cursor: 'pointer',
+          fontSize: '15px'
+        }}>
           장바구니 구매하기
         </button>
       </Link>
     </footer>
   );
 }
-
-export default Footer;
-
-const footerWrapperStyle = {
-  display: 'flex',
-  justifyContent: 'center', 
-  width: '100%',
-  padding: '0 20px 60px 20px',
-};
-
-const linkStyle = {
-  width: '100%',
-  maxWidth: '800px', 
-  display: 'block', 
-  textDecoration: 'none',
-};
-
-const bottomPurchaseButtonStyle = {
-  width: '100%', 
-  padding: '13px 0',
-  backgroundColor: 'white',
-  border: '2px solid #0085FF',
-  color: '#0085FF',
-  borderRadius: '8px',
-  cursor: 'pointer',
-  fontSize: '16px',
-  transition: '0.2s',
-};
